6c4c886fb8ada68d37810e2cf1a1f9a5c5046a3b
shk1999 <shayan000karimi@gmail.com>
2025-06-10 15:54:08 +0330

    update(monarch-mklive): remove EUID check since there is not need

--- monarch-mklive.sh
+++ monarch-mklive.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 # Monarch Linux ISO builder script
-# 
-# Make sure the script is running with root privileges
-
-if ! [[ $EUID -ne 0 ]]; then
-  echo "Please run as root."
-  exit 1
-fi
-
 # Run the build 
-sudo void-mklive \
+
+sudo ./mkiso.sh \
   -a x86_64 \
   -o . \
   -p "Monarch Linux" \
